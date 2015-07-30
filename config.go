1c71fc53c0c52aa3518d154ce923229ce6d7f18f
Knut Ahlers <knut@ahlers.me>
2015-07-30 17:25:10 +0200

    Added env reader for parameters

--- config.go
+++ config.go
@@ -11,10 +11,10 @@ type config struct {
 	PasswordSalt string `env:"passwordSalt" flag:"password-salt" description:"A random unique salt for encrypting the passwords"`
 	UsernameSalt string `env:"usernameSalt" flag:"username-salt" description:"A random unique salt for encrypting the usernames"`
 	Storage      string `env:"storage" flag:"storage" default:"local:///./data" description:"Configuration for storage adapter (see README.md)"`
-	Listen       string `flag:"listen" default:":3000" description:"IP and port to listen on"`
+	Listen       string `flag:"listen" env:"listen" default:":3000" description:"IP and port to listen on"`
 
-	CookieSigningKey string `flag:"cookie-authkey" description:"Key used to authenticate the session"`
-	CookieEncryptKey string `flag:"cookie-encryptkey" description:"Key used to encrypt the session"`
+	CookieSigningKey string `flag:"cookie-authkey" env:"authkey" description:"Key used to authenticate the session"`
+	CookieEncryptKey string `flag:"cookie-encryptkey" env:"encryptkey" description:"Key used to encrypt the session"`
 
 	VersionAndQuit bool `flag:"version" default:"false" description:"Show version and quit"`
 }
