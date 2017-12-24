d64fee60c88222835c9c1358b3475d662c9fc368
Knut Ahlers <knut@ahlers.me>
2017-12-24 11:55:52 +0100

    Replace insecure password hashing
    Prior this commit passwords were hashed with a static salt and using the
    SHA1 hashing function. This could lead to passwords being attackable in
    case someone gets access to the raw data stored inside the database.
    This commit introduces password hashing using bcrypt hashing function
    which addresses this issue.
    
    Old passwords are not automatically re-hashed as they are unknown.
    Replacing the old password scheme is not that easy and needs #10 to be
    solved. Therefore the old hashing scheme is kept for compatibility
    reason.
    
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- register.go
+++ register.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"crypto/sha1"
 	"fmt"
 	"net/http"
 	"strings"
+
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/flosch/pongo2"
 	"github.com/gorilla/sessions"
@@ -12,10 +13,9 @@ import (
 
 func registerHandler(res http.ResponseWriter, r *http.Request, session *sessions.Session, ctx *pongo2.Context) (*string, error) {
 	var (
-		username       = strings.ToLower(r.FormValue("username"))
-		password       = r.FormValue("password")
-		passwordCheck  = r.FormValue("password_repeat")
-		hashedPassword = fmt.Sprintf("%x", sha1.Sum([]byte(cfg.PasswordSalt+password)))
+		username      = strings.ToLower(r.FormValue("username"))
+		password      = r.FormValue("password")
+		passwordCheck = r.FormValue("password_repeat")
 	)
 
 	if username == "" || password == "" || password != passwordCheck {
@@ -27,8 +27,15 @@ func registerHandler(res http.ResponseWriter, r *http.Request, session *sessions
 		return stringPointer("register.html"), nil
 	}
 
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Printf("ERR: Unable to hash users password: %s\n", err)
+		(*ctx)["error"] = true
+		return stringPointer("register.html"), nil
+	}
+
 	d := dataObject{}
-	d.MetaData.Password = hashedPassword
+	d.MetaData.Password = string(hashedPassword)
 	data, _ := d.GetData()
 
 	if err := storage.Write(createUserFilename(username), data); err != nil {
